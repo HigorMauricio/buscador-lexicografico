4acabae5fbf4f4d7d48e43e1e6e29982e83af0d7
HigorMauricio <higormauriciosilva@gmail.com>
2025-12-14 14:35:00 -0300

    Criação da função de normalização das strings

--- src/buscaPalavra.c
+++ src/buscaPalavra.c
@@ -1,3 +1,18 @@
+#include "buscaPalavra.h"
+
 char *normalizaString(char* palavra){
-    
+    if(!palavra){
+        return NULL;
+    }
+
+    int fim = 0;
+    for(int i=0; palavra[i]!='\0'; i++){
+        if(isalnum(palavra[i])){
+            palavra[i] = tolower(palavra[i]);
+            fim ++;
+        }
+    }
+    palavra[fim] = '\0';
+
+    return palavra;
 }
